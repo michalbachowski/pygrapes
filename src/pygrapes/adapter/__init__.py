8bf2f58bfe13c5d2fd1969991b1ccbec52e65a42
Michał - MiB - Bachowski <mib@heroes.net.pl>
2013-02-11 14:22:49 +0100

    Added abstract class for all adapters: adapter.abstract.Abstract

--- src/pygrapes/adapter/__init__.py
+++ src/pygrapes/adapter/__init__.py
@@ -5,4 +5,6 @@ __date__ = "$2011-01-22 12:02:41$"
 
 #from zeromq import Zmq
 #from amqp import Amqp
+from abstract import Abstract as Base
 
+__all__ = ['Base']
---
+++ src/pygrapes/adapter/abstract.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Base class for all adapters
+"""
+
+
+class Abstract(object):
+    """
+    Base abstract class for all adapters
+    """
+
+    def send(self, route, message, deferred):
+        """
+        Method to be implemented by all adapters.
+        Sends message to given route. Accepts 'deferred' keyword argument.
+        """
+        raise NotImplementedError()
+
+    def attach_listener(self, route, callback):
+        """
+        Method to be implemented by all adapters.
+        Binds callback with message with given route.
+        When message with given route was received given callback is called
+        with 'deferred' keyword argument that is used to pass back response.
+        """
+        raise NotImplementedError()
+    
+    def detach_listener(self, route):
+        """
+        Method to be implemented by all adapters.
+        Unbinds callback from message with given route.
+        """
+        raise NotImplementedError()
+
+    def ack(self, message):
+        """
+        Method may be implemented by some adapters.
+        Sends information that message was processed.
+        """
+        pass
