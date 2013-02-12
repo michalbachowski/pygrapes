a516bb6c4eed0535f1b6394f0b2f4cdf9d81211d
Michał - MiB - Bachowski <mib@heroes.net.pl>
2013-02-12 15:02:00 +0100

    Allowed loading 'Local' adapter directly from pygrapes.adapter module; Renamed 'Base' class to 'Abstract'

--- src/pygrapes/adapter/__init__.py
+++ src/pygrapes/adapter/__init__.py
@@ -5,6 +5,7 @@ __date__ = "$2011-01-22 12:02:41$"
 
 #from zeromq import Zmq
 #from amqp import Amqp
-from abstract import Abstract as Base
+from abstract import Abstract
+from local import Local
 
-__all__ = ['Base']
+__all__ = ['Abstract', 'Local']
