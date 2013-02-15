ab55f28592956cc6c9abbea31c2b0d66e13cddc1
Michał - MiB - Bachowski <mib@heroes.net.pl>
2013-02-15 14:38:27 +0100

    Load conditionally all available adapters in order to make them available right inside pygrapes.adapter module

--- src/pygrapes/adapter/__init__.py
+++ src/pygrapes/adapter/__init__.py
@@ -3,7 +3,19 @@
 __author__ = "mib"
 __date__ = "$2011-01-22 12:02:41$"
 
-from abstract import Abstract
-from local import Local
+from pygrapes.util import not_implemented
+from pygrapes.adapter.abstract import Abstract
+from pygrapes.adapter.local import Local
 
-__all__ = ['Abstract', 'Local']
+try:
+    from pygrapes.adapter.zeromq import Zmq
+except ImportError:
+    Zmq = not_implemented('A working pyzmq lib is required!')
+
+try:
+    from pygrapes.adapter.amqp import Amqp
+except ImportError:
+    Amqp = not_implemented('A working amqplib lib is required!')
+
+
+__all__ = ['Abstract', 'Amqp', 'Local', 'Zmq']
