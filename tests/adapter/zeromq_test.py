70128a66885f88808078ea34f487272919d81faf
Michał Bachowski <michal@bachowski.pl>
2013-02-22 12:45:55 -0800

    Refactored zeromq_test to use 'mock' mocking library

--- tests/adapter/zeromq_test.py
+++ tests/adapter/zeromq_test.py
@@ -4,15 +4,14 @@
 ##
 # hack for loading modules
 #
-import _path
-_path.fix()
+from _path import fix, mock
+fix()
 
 ##
 # python standard library
 #
 from functools import partial
 import unittest
-import mox
 
 ##
 # ZeroMQ modules
@@ -28,24 +27,27 @@ from promise import Deferred
 ##
 # pygrapes modules
 #
+from mock_helper import IsA, IsCallable, results
 from pygrapes.adapter import Zmq
+
 
 @unittest.skipIf(zmq is None, 'Missing zmq Python library')
 class ZmqAdapterTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.mox = mox.Mox()
-        self.ctx = self.mox.CreateMock(zmq.Context)
-        self.socket = self.mox.CreateMock(zmq.Socket)
-        self.deferred = self.mox.CreateMock(Deferred)
+        self._ctx = self.mox.CreateMock(zmq.Context)
+        self._socket = self.mox.CreateMock(zmq.Socket)
+        self._deferred = self.mox.CreateMock(Deferred)
+
+        self.ctx = mock.Mock()
+        self.socket = mock.Mock()
+        self.deferred = mock.Mock()
 
     def setUpConnect(self):
-        self.ctx.socket(mox.IsA(int)).AndReturn(self.socket)
-        self.socket.connect(mox.IsA(str))
+        self.ctx.socket = mock.MagicMock(return_value=self.socket)
 
     def setUpBind(self):
-        self.ctx.socket(mox.IsA(int)).AndReturn(self.socket)
-        self.socket.bind(mox.IsA(str))
+        self.ctx.socket = mock.MagicMock(return_value=self.socket)
 
     def tearDown(self):
         self.mox.UnsetStubs()
@@ -64,59 +66,42 @@ class ZmqAdapterTestCase(unittest.TestCase):
 
     def test_connects_expects_valid_context_instance_1(self):
         self.setUpConnect()
-        self.mox.ReplayAll()
-
         Zmq(context=self.ctx).connect()
-
-        self.mox.VerifyAll()
+        self.ctx.socket.assert_called_once_with(IsA(int))
+        self.socket.connect.assert_called_once_with(IsA(str))
 
     def test_connect_return_instance_of_self(self):
-        self.setUpConnect()
-        self.mox.ReplayAll()
-
         self.assertTrue(isinstance(Zmq(context=self.ctx).connect(), Zmq))
-
-        self.mox.VerifyAll()
 
     def test_connect_will_not_instantinate_socket_twice(self):
         self.setUpConnect()
-        self.mox.ReplayAll()
 
         z = Zmq(context=self.ctx)
         self.assertTrue(isinstance(z.connect(), Zmq))
         self.assertTrue(isinstance(z.connect(), Zmq))
-
-        self.mox.VerifyAll()
+        self.ctx.socket.assert_called_once_with(IsA(int))
+        self.socket.connect.assert_called_once_with(IsA(str))
 
     def test_connects_expects_valid_config_instance(self):
-        self.setUpConnect()
-        self.mox.ReplayAll()
-
         self.assertRaises(AttributeError,
                 Zmq(context=self.ctx, config='a').connect)
 
-        self.mox.VerifyAll()
-
     def test_connects_expects_valid_config_instance_1(self):
         self.setUpConnect()
-        cfg = self.mox.CreateMock(dict)
-        cfg.iteritems().AndReturn([])
-        self.mox.ReplayAll()
+        cfg = mock.Mock()
+        cfg.iteritems = mock.MagicMock(return_value = [])
 
         Zmq(context=self.ctx, config=cfg).connect()
-
-        self.mox.VerifyAll()
+        cfg.iteritems.assert_called_once_with()
 
     def test_connects_triggers_socket_configuration(self):
         self.setUpConnect()
-        cfg = self.mox.CreateMock(dict)
-        cfg.iteritems().AndReturn({'a': 1}.iteritems())
-        self.socket.setsockopt('a', 1)
-        self.mox.ReplayAll()
+        cfg = mock.Mock()
+        cfg.iteritems = mock.MagicMock(return_value = {'a': 1}.iteritems())
 
         Zmq(context=self.ctx, config=cfg).connect()
 
-        self.mox.VerifyAll()
+        self.socket.setsockopt.assert_called_once_with('a', 1)
 
 
     def test_send_expects_3_args(self):
@@ -131,70 +116,70 @@ class ZmqAdapterTestCase(unittest.TestCase):
 
     def test_send_passes_response_from_socket_recv_to_deferred_resolve(self):
         self.setUpConnect()
-        self.socket.send(mox.IsA(str), mox.IsA(int))
-        self.socket.send(mox.IsA(str))
-        self.socket.recv().AndReturn('resp')
-        self.deferred.resolve('resp')
-        self.mox.ReplayAll()
+        self.socket.recv = mock.MagicMock(return_value = 'resp')
 
         Zmq(context=self.ctx).connect().send('route', 'message',
                 self.deferred)
 
-        self.mox.VerifyAll()
+        expected = [mock.call(IsA(str), IsA(int)), mock.call(IsA(str))]
+        self.assertEqual(self.socket.send.call_args_list, expected)
+        self.socket.recv.assert_called_once_with()
+        self.deferred.resolve.assert_called_once_with('resp')
+
 
     def test_serve_will_not_initialize_already_initialized_socket(self):
         self.setUpConnect()
-        self.mox.ReplayAll()
 
         z = Zmq(context=self.ctx)
         z.connect()
         self.assertTrue(z.serve() is None)
-
-        self.mox.VerifyAll()
+        self.socket.connect.assert_called_once_with(IsA(str))
+        self.assertEqual(self.socket.bind.call_count, 0)
 
     def test_serve_will_work_until_exception_raises(self):
         self.setUpBind()
-        self.socket.recv().WithSideEffects(terminate_server)
-        self.mox.ReplayAll()
+        self.socket.recv = mock.MagicMock(side_effect=KeyboardInterrupt)
         self.assertRaises(KeyboardInterrupt, Zmq(context=self.ctx).serve)
-        self.mox.VerifyAll()
+
+        self.socket.bind.assert_called_once_with(IsA(str))
+        self.socket.recv_assert_called_once_with()
 
     def test_serve_expects_handler_for_given_route(self):
         self.setUpBind()
-        self.socket.recv().AndReturn('route')
-        self.socket.recv().AndReturn('msg')
-        self.mox.ReplayAll()
+
+        self.socket.recv = mock.MagicMock(side_effect=results('route', 'msg'))
         z = Zmq(context=self.ctx)
         self.assertRaises(KeyError, z.serve)
-        self.mox.VerifyAll()
 
-    def test_serve_deferred_instance_returned_from_handler(self):
-        self.setUpBind()
-        c = self.mox.CreateMockAnything()
-        c(mox.IsA(str))
-        self.socket.recv().AndReturn('route')
-        self.socket.recv().AndReturn('msg')
-        self.mox.ReplayAll()
-        z = Zmq(context=self.ctx)
-        z.attach_listener('route', c)
-        self.assertRaises(AttributeError, z.serve)
-        self.mox.VerifyAll()
+        expected = [mock.call(), mock.call()]
+        self.assertEqual(self.socket.recv.call_args_list, expected)
 
-    def test_serve_expects_zmq_messages_consisted_on_2_parts(self):
+    def test_serve_expects_deferred_instance_returned_from_handler(self):
         self.setUpBind()
-        c = self.mox.CreateMockAnything()
-        c('msg').AndReturn(self.deferred)
-        self.socket.recv().AndReturn('route')
-        self.socket.recv().AndReturn('msg')
-        self.deferred.done(mox.IsA(object)).WithSideEffects(terminate_server)
-        self.mox.ReplayAll()
+        c = mock.MagicMock(return_value=self.deferred)
+
+        self.socket.recv = mock.MagicMock(side_effect=results('route', 'msg',
+                KeyboardInterrupt()))
+
         z = Zmq(context=self.ctx)
         z.attach_listener('route', c)
         self.assertRaises(KeyboardInterrupt, z.serve)
-        self.mox.VerifyAll()
 
-def terminate_server(*args, **kwargs):
-    raise KeyboardInterrupt()
+        c.assert_called_once_with(IsA(str))
+        self.deferred.done.assert_called_once_with(IsCallable())
+
+    def test_serve_expects_zmq_messages_consisted_on_2_parts(self):
+        self.setUpBind()
+        c = mock.MagicMock(return_value=self.deferred)
+
+        self.socket.recv = mock.MagicMock(side_effect=results('route', 'msg'))
+        self.deferred.done = mock.MagicMock(side_effect=KeyboardInterrupt)
+        z = Zmq(context=self.ctx)
+        z.attach_listener('route', c)
+        self.assertRaises(KeyboardInterrupt, z.serve)
+
+        c.assert_called_once_with(IsA(str))
+
 
 if "__main__" == __name__:
     unittest.main()
